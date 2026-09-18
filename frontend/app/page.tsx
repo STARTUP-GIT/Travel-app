9fe920f440227c7a6bcf61f060d7b2bfec08c2ea
Sathwik MS <sathwikms11@gmail.com>
2026-09-18 20:55:28 +0530

    main check

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 
 
 export default function Home() {
-  return <div className="w-full h-full min-h-screen flex justify-center items-center">
-      Travel app landing page
+  return <div className="w-full h-full min-h-screen flex justify-center items-center text-2xl">
+    Travel APP ( WHICH HELPS YOU TO PLAN YOUR TRAVEL )
   </div>
 }
